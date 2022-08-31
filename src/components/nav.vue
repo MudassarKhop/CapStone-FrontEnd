2000dad26840ad2f3772b337f0c813a8784f88ca
MudassarKhop <khopatkarmudassar@gmail.com>
2022-08-31 08:36:20 +0200

    before pull

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,50 +1,45 @@
 <template>
+	<!-- Navbar -->
 	<nav id="nav" class="navbar navbar-expand-lg">
-		<div class="container-fluid">
-			<router-link to="/" class="navbar-brand text-light"
-				><i class="fa-solid fa-plane-up"></i> OXFORD-AIRLINES</router-link
-			>
-
-			<button
-				class="navbar-toggler"
-				type="button text-light"
-				data-bs-toggle="collapse"
-				data-bs-target="#navbarSupportedContent"
-				aria-controls="navbarSupportedContent"
-				aria-expanded="false"
-				aria-label="Toggle navigation"
-			>
-				<span class="navbar-icon border-none"
-					><i class="fa-solid fa-bars"></i
-				></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul id="list" class="navbar-nav me-auto mb-2 mb-lg-0">
-					<li class="nav-item">
-						<router-link to="/">Home</router-link>
-					</li>
-					<li class="nav-item">
-						<router-link to="/about">About</router-link>
-					</li>
-					<li class="nav-item">
-						<router-link to="/flights">Flights</router-link>
-					</li>
-					<li class="nav-item">
-						<router-link to="/jets">Jets</router-link>
-					</li>
-					<li class="nav-item">
-						<router-link to="/contactus">Contact Us</router-link>
-					</li>
-				</ul>
-				<form class="d-flex" role="search">
-					<input
-						class="form-control me-2"
-						type="search"
-						placeholder="Search"
-						aria-label="Search"
-					/>
-					<button class="btn text-light" type="submit">Search</button>
-				</form>
+		<button
+			class="navbar-toggler"
+			type="button"
+			data-mdb-toggle="collapse"
+			data-mdb-target="#navbarSupportedContent"
+			aria-controls="navbarSupportedContent"
+			aria-expanded="false"
+			aria-label="Toggle navigation"
+		>
+			<i class="fas fa-bars"></i>
+		</button>
+		<div class="container d-flex justify-content-center">
+			<div class="row">
+				<div class="col-12 d-flex justify-content-center mb-3">
+					<router-link to="/" class="navbar-brand text-light"
+						><i class="fa-solid fa-plane-up"></i> OXFORD-AIRLINES</router-link
+					>
+				</div>
+				<div class="col-12 d-flex justify-content-center">
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav align-items-center mx-auto">
+							<li class="nav-item">
+								<router-link to="/">Home</router-link>
+							</li>
+							<li class="nav-item">
+								<router-link to="/about">About</router-link>
+							</li>
+							<li class="nav-item">
+								<router-link to="/flights">Flights</router-link>
+							</li>
+							<li class="nav-item">
+								<router-link to="/jets">Jets</router-link>
+							</li>
+							<li class="nav-item">
+								<router-link to="/contactus">Contact Us</router-link>
+							</li>
+						</ul>
+					</div>
+				</div>
 			</div>
 		</div>
 	</nav>
